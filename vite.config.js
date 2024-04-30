9cf93b7858620666fe9ce16735bd5dbaaa702b19
seducerooo <>
2024-04-30 15:58:32 +0330

    laravel project created successfully && breeze package installed with options such as ssr and vue && inertia server side installed&& inertia client side installed && bootstrap installed

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/js/app.js',
+                'resources/sass/app.scss'
+            ],
+            ssr: 'resources/js/ssr.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
